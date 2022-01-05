5b085587271ce2fedcb0407760c84c722ff3345e
Bob F <bobfdev@gmail.com>
2022-01-05 16:50:06 -0600

    updated files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ForgotPassword from './pages/ForgotPassword';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/forgotpassword' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
